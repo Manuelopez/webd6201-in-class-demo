55d4c17027e6a85af44ebf9994a0bf5b62369dec
Manuel Lopez <mp.arturolopez@gmail.com>
2023-02-16 15:48:33 -0500

    ice 6

--- ICE6/scripts/main.js
+++ ICE6/scripts/main.js
@@ -20,6 +20,14 @@
     console.log('Projects Page');
   }
 
+  function AddContact(fullName, contactNumber, emailAddress) {
+    let contact = new core.Contact(fullName, contactNumber, emailAddress);
+    if (contact.serialize()) {
+      let key = contact.Name.substring(0, 1) + Date.now();
+      localStorage.setItem(key, contact.serialize());
+    }
+  }
+
   function DisplayContacts() {
     console.log('Contact Us Page');
 
@@ -35,15 +43,7 @@
       event.preventDefault();
       if (subscribeCheckbox.checked) {
         // If user subscribes, store the contact in localStorage
-        let contact = new Contact(
-          fullName.value,
-          contactNumber.value,
-          emailAddress.value
-        );
-        if (contact.serialize()) {
-          let key = contact.Name.substring(0, 1) + Date.now();
-          localStorage.setItem(key, contact.serialize());
-        }
+        AddContact(fullName.value, contactNumber.value, emailAddress.value);
       }
 
       return false;
@@ -62,7 +62,7 @@
       // for every key in the keys collection
       for (const key of keys) {
         let contactData = localStorage.getItem(key); // Get localStorage data value related to the key
-        let contact = new Contact();
+        let contact = new core.Contact();
         console.log(key.toString());
 
         contact.deserialize(contactData);
@@ -73,8 +73,8 @@
                     <td class="text-center">${contact.Name}</td>
                     <td class="text-center">${contact.ContactNumber}</td>
                     <td class="text-center">${contact.EmailAddress}</td>
-                    <td class="text-center"><button value=""  class="btn btn-primary btn-sm edit"><i class="fas fa-edit fa-sm"></i>&nbsp; Edit</button></td>
-                    <td class="text-center"><button value="" onclick="return DeleteContactList('${key}')" class="btn btn-danger btn-sm delete"><i class="fas fa-trash-alt fa-sm"></i>&nbsp; Delete</button></td>
+                    <td class="text-center"><button value="${key}"  class="btn btn-primary btn-sm edit"><i class="fas fa-edit fa-sm"></i>&nbsp; Edit</button></td>
+                    <td class="text-center"><button value="${key}" onclick="return DeleteContactList('${key}')" class="btn btn-danger btn-sm delete"><i class="fas fa-trash-alt fa-sm"></i>&nbsp; Delete</button></td>
                 </tr>
                 `;
 
@@ -82,6 +82,67 @@
       }
 
       contactList.innerHTML = data;
+
+      $('button.edit').on('click', function () {
+        location.href = `edit.html#` + $(this).val();
+      });
+    }
+
+    $('#addButton').on('click', () => {
+      console.log('here');
+      location.href = 'edit.html#Add';
+    });
+    contactList.innerHTML = data;
+  }
+
+  function DisplayEditPage() {
+    let page = location.hash.substring(1);
+    switch (page) {
+      case 'Add':
+        {
+          $('#welcome').text('WEBD6201 Demo Add Contact');
+
+          $('#editButton').html(`<i class="fas fa-plus-circle fa-lg"></i> Add`);
+
+          $('#editButton').on('click', (event) => {
+            event.preventDefault();
+
+            // get form information (name, contact number, email address)
+            AddContact(fullName.value, contactNumber.value, emailAddress.value);
+
+            location.href = 'contact-list.html';
+            // redirect to contact-;ist
+          });
+        }
+        break;
+      default:
+        {
+          // get contact ingo from local storeage
+
+          let contact = new core.Contact();
+          contact.deserialize(localStorage.getItem(page));
+          // display contatct info in edit form
+          $('#fullName').val(contact.Name);
+          $('#contactNumber').val(contact.ContactNumber);
+          $('#emailAddress').val(contact.EmailAddress);
+
+          // when edit button pressed, update the contact
+          $('#editButton').on('click', (event) => {
+            event.preventDefault();
+
+            // get all changes from the form
+            contact.Name = $('#fullName').val();
+            contact.ContactNumber = $('#contactNumber').val();
+            contact.EmailAddress = $('#emailAddress').val();
+
+            // replacce the changes in localstorage
+            localStorage.setItem(page, contact.serialize());
+
+            // go back to contact-list.htm
+            location.href = 'contact-list.html';
+          });
+        }
+        break;
     }
   }
 
@@ -107,6 +168,9 @@
         break;
       case 'References - WEBD6201 Demo':
         DisplayReferences();
+        break;
+      case 'Edit - WEBD6201 Demo':
+        DisplayEditPage();
         break;
     }
   }
